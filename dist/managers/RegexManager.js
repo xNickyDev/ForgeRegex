42dbc12bdfe4d58e37ee7694bd4010517e0c7fd3
xNickyDev <xnickydev@gmail.com>
2024-11-13 01:36:44 +0100

    Added more debug logs

--- dist/managers/RegexManager.js
+++ dist/managers/RegexManager.js
@@ -5,15 +5,20 @@ const discord_js_1 = require("discord.js");
 class RegexManager {
     regexes = new discord_js_1.Collection();
     create(name, regex) {
+        console.log(`Created ${name}: ${regex}`);
         this.regexes.set(name, new RegExp(regex));
+        console.log(`RegExp: ${new RegExp(regex)}`);
     }
     get(name) {
+        console.log(`Got: ${name}`);
         return this.regexes.get(name);
     }
     delete(name) {
+        console.log(`Deleted: ${name}`);
         this.regexes.delete(name);
     }
     clear() {
+        console.log("Cleared all regexes");
         this.regexes.clear();
     }
 }
--- src/managers/RegexManager.ts
+++ src/managers/RegexManager.ts
@@ -4,18 +4,23 @@ export class RegexManager {
     private readonly regexes = new Collection<string, RegExp>()
 
     public create(name: string, regex: string) {
+        console.log(`Created ${name}: ${regex}`)
         this.regexes.set(name, new RegExp(regex))
+        console.log(`RegExp: ${new RegExp(regex)}`)
     }
 
     public get(name: string) {
+        console.log(`Got: ${name}`)
         return this.regexes.get(name)
     }
 
     public delete(name: string) {
+        console.log(`Deleted: ${name}`)
         this.regexes.delete(name)
     }
 
     public clear() {
+        console.log("Cleared all regexes")
         this.regexes.clear()
     }
 }
