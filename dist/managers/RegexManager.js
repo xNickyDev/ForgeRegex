ef4cd87f4fc084a40c81611f89296a4966ef36d3
xNickyDev <xnickydev@gmail.com>
2024-11-13 15:08:56 +0100

    added console logs

--- dist/managers/RegexManager.js
+++ dist/managers/RegexManager.js
@@ -1,15 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RegexManager = void 0;
+console.log("Manager File Executed", true);
 class Manager {
     regexes;
     constructor() {
         this.regexes = new Map();
+        console.log("New Map:", this.regexes);
     }
     get(name) {
+        console.log("Get Regex:", this.regexes.get(name));
         return this.regexes.get(name);
     }
     delete(name) {
+        console.log("Delete Regex", this.regexes.delete(name));
         this.regexes.delete(name);
     }
 }
@@ -17,9 +21,11 @@ class RegexManager extends Manager {
     current = null;
     constructor() {
         super();
+        console.log("Constructor Regexes", this.current);
     }
     create(name, regexString) {
         const regex = new RegExp(regexString);
+        console.log("Create Regex:", regex);
         this.regexes.set(name, regex);
         this.current = regex;
     }
--- src/managers/RegexManager.ts
+++ src/managers/RegexManager.ts
@@ -1,15 +1,20 @@
+console.log("Manager File Executed", true)
+
 class Manager {
     public regexes: Map<string, RegExp>
 
     constructor() {
         this.regexes = new Map()
+        console.log("New Map:", this.regexes)
     }
 
     public get(name: string) {
+        console.log("Get Regex:", this.regexes.get(name))
         return this.regexes.get(name)
     }
 
     public delete(name: string) {
+        console.log("Delete Regex", this.regexes.delete(name))
         this.regexes.delete(name)
     }
 }
@@ -19,10 +24,12 @@ export class RegexManager extends Manager {
 
     constructor() {
         super()
+        console.log("Constructor Regexes", this.current)
     }
 
     public create(name: string, regexString: string) {
         const regex = new RegExp(regexString)
+        console.log("Create Regex:", regex)
         this.regexes.set(name, regex)
         this.current = regex
     }
