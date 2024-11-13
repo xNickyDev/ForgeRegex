97918c795f834bb35dc82b8546e8b2c90212c31d
xNickyDev <xnickydev@gmail.com>
2024-11-13 01:25:06 +0100

    test

--- dist/structures/Context.js
+++ dist/structures/Context.js
@@ -2,8 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Context = void 0;
 const forgescript_1 = require("@tryforge/forgescript");
+const RegexManager_1 = require("../managers/RegexManager");
 class Context extends forgescript_1.Context {
     regexes;
+    constructor(runtime) {
+        super(runtime);
+        this.regexes = new RegexManager_1.RegexManager();
+    }
 }
 exports.Context = Context;
 //# sourceMappingURL=Context.js.map
--- src/structures/Context.ts
+++ src/structures/Context.ts
@@ -1,6 +1,11 @@
-import { Context as ctx } from "@tryforge/forgescript"
+import { Context as ctx, IRunnable } from "@tryforge/forgescript"
 import { RegexManager } from "../managers/RegexManager"
 
 export class Context extends ctx {
     regexes?: RegexManager
+
+    constructor(runtime: IRunnable) {
+        super(runtime)
+        this.regexes = new RegexManager()
+    }
 }
