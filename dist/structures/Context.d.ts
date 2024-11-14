057b8f487ef56d04d7332283049efa8aa50c4e7e
xNickyDev <xnickydev@gmail.com>
2024-11-15 00:44:59 +0100

    Removed unused enum

--- dist/structures/Context.d.ts
+++ dist/structures/Context.d.ts
@@ -3,12 +3,4 @@ import { RegexManager } from "../managers/RegexManager";
 export declare class Context extends ctx {
     regexes?: RegexManager;
 }
-export declare enum RegexFlags {
-    global = "g",
-    ignoreCase = "i",
-    multiline = "m",
-    dotAll = "s",
-    unicode = "u",
-    sticky = "y"
-}
 //# sourceMappingURL=Context.d.ts.map
