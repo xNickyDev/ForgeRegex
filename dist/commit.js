595add6697ae898132bb8c21f8048fa71accba41
xNickyDev <xnickydev@gmail.com>
2025-06-01 02:55:54 +0200

    Restructured changelogs.json

--- dist/commit.js
+++ dist/commit.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
 const fs_1 = require("fs");
-const path_1 = require("path");
 const prompt_1 = __importDefault(require("./functions/prompt"));
+const path_1 = require("path");
 const path = "./metadata";
 if (!(0, fs_1.existsSync)(path))
     (0, fs_1.mkdirSync)(path);
@@ -29,8 +29,15 @@ async function main() {
     const fileName = (0, path_1.join)(path, "changelogs.json");
     const json = (0, fs_1.existsSync)(fileName) ? JSON.parse((0, fs_1.readFileSync)(fileName, "utf-8")) : {};
     json[version] ??= [];
+    for (const key in json) {
+        json[key] = json[key].map(str => typeof str === "string" ? { message: str } : str);
+    }
     if (!skip) {
-        json[version].unshift(msg);
+        json[version].unshift({
+            message: msg,
+            timestamp: new Date(),
+            author: (0, child_process_1.execSync)("git config user.name").toString().trim()
+        });
         (0, fs_1.writeFileSync)(fileName, JSON.stringify(json), "utf-8");
     }
     const branch = await (0, prompt_1.default)("Write the branch name to push to (defaults to dev): ") || "dev";
